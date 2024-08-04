8ba3cbfc274cb2bd7b8244d778e9120b3ff0e0e0
Randy Souza <rsouza@mathworks.com>
2024-08-04 13:13:41 -0400

    Install dependencies

--- buildfile.m
+++ buildfile.m
@@ -1,9 +1,25 @@
 function plan = buildfile
     import matlab.buildtool.tasks.TestTask;
 
+    installDependencies;
+
     plan = buildplan(localfunctions);
 
     plan("test") = TestTask;
+    plan("package") = fileexchange.tasks.PackageTask;
+    plan("publish") = fileexchange.tasks.PublishTask;
+
+    plan("publish").Dependencies = "package";
 
     plan.DefaultTasks = "test";
+end
+
+function installDependencies
+    repositories = mpmListRepositories;
+
+    if isempty(repositories)
+        mpmAddRepository("local", getenv("MPM_REPOSITORY_PATH"));
+    end
+
+    mpminstall("FileExchangeToolbox", Prompt=false, Verbosity="quiet");
 end
