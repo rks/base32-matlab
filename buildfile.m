e4770194eebe894c363dfdd1dcd19cb6814560a8
Randy Souza <rsouza@mathworks.com>
2024-12-18 22:20:04 -0500

    Clean up scratch files

--- buildfile.m
+++ buildfile.m
@@ -1,30 +1,9 @@
 function plan = buildfile
     import matlab.buildtool.tasks.TestTask;
 
-    installDependencies;
-
     plan = buildplan(localfunctions);
 
     plan("test") = TestTask;
-    plan("package") = fileexchange.tasks.PackageTask;
-    plan("publish") = fileexchange.tasks.PublishTask;
-
-    plan("publish").Dependencies = "package";
 
     plan.DefaultTasks = "test";
-end
-
-function installDependencies
-    % Can't use MPM functions in non-interactive contexts like GitHub
-    % Actions.
-    %
-    % repositories = mpmListRepositories;
-    % 
-    % if isempty(repositories)
-    %     mpmAddRepository("local", getenv("MPM_REPOSITORY_PATH"));
-    % end
-    % 
-    % mpminstall("FileExchangeToolbox", Prompt=false, Verbosity="quiet");
-
-    addpath(fullfile(getenv("MPM_REPOSITORY_PATH"), "FileExchangeToolbox", "toolbox"));
 end
